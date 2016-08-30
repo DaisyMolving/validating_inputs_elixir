e09fd7e93320c1731b58cfe28442bed95c183423
Daisy Molving <daisymolving@gmail.com>
2016-08-30 15:02:47 +0100

    created private functions that house each type of capture_io function, which are passed relevant functions from ValidatingInputs module along with their relevant arguments

--- test/validating_inputs_test.exs
+++ test/validating_inputs_test.exs
@@ -4,62 +4,70 @@ defmodule ValidatingInputsTest do
   doctest ValidatingInputs
 
   test "prints out each request" do
-    assert capture_io(fn ->
-      ValidatingInputs.print_input_request(:first_name)
-    end) =~ "first name"
-    assert capture_io(fn ->
-      ValidatingInputs.print_input_request(:last_name)
-    end) =~ "last name"
-    assert capture_io(fn ->
-      ValidatingInputs.print_input_request(:zip_code)
-    end) =~ "zip code"
-    assert capture_io(fn ->
-      ValidatingInputs.print_input_request(:employee_id)
-    end) =~ "employee id"
+    func = &ValidatingInputs.print_input_request/1
+    result = capture_output(func, :first_name)
+    assert result =~ "Please enter your"
   end
 
   test "a first name or last name must be at least 2 letters long" do 
-    assert capture_io("retry", fn ->
-	ValidatingInputs.validate_type("D", :first_name) 
-    end) =~ "first name must be at least two characters long" 
-    assert ValidatingInputs.validate_type("Molving", :last_name) == "Molving"
+    func = &ValidatingInputs.validate_type/2
+    result = capture_invalid_message(func, "D", "retry", :first_name)
+    assert result =~ "first name must be at least two characters long"
   end 
 
   test "a zip code must be completely numerical" do
-    assert capture_io("12345", fn ->
-       ValidatingInputs.validate_type("ABC4E", :zip_code) 
-    end) =~ "zip code must be numerical"
-    assert ValidatingInputs.validate_type("12345", :zip_code) == "12345"
+    func = &ValidatingInputs.validate_type/2
+    result = capture_invalid_message(func, "ABC4E", "12345", :zip_code)
+    assert result =~ "zip code must be numerical"
   end
 
   test "an employee id must start with two letters, then a hyphen, then four numbers" do
-    assert capture_io("ZZ-7890", fn ->
-      ValidatingInputs.validate_type("AA1234", :employee_id)
-    end) =~ "employee id must have two letters followed by a hyphen, then four numbers"
-    assert ValidatingInputs.validate_type("AA-1234", :employee_id) == "AA-1234"
+    func = &ValidatingInputs.validate_type/2
+    result = capture_invalid_message(func, "AA1234", "ZZ-7890", :employee_id)
+    assert result =~ "employee id must have two letters followed by a hyphen, then four numbers"
   end
 
-  test "invalid_empty message is printed when field is empty" do
-    assert capture_io("retry", fn -> ValidatingInputs.validate_not_empty("", :first_name) 
-    end) == "That is an invalid input, first_name cannot be empty. Please try again: "
+  test "retry_for_empty_field message is printed when field is empty" do
+    func = &ValidatingInputs.validate_not_empty/2
+    result = capture_invalid_message(func, "", "retry", :first_name)
+    assert result =~ "That is an invalid input, first_name cannot be empty. Please try again: "
   end 
 
-  test "invalid_empty message takes an input" do
-    input = "retry"
-    assert capture_io([input: input, capture_prompt: false], fn ->
-      IO.write(ValidatingInputs.retry_for_empty_field(:first_name))
-    end) == "retry"
+  test "retry_for_empty_field takes an input" do
+    func = &ValidatingInputs.validate_not_empty/2
+    result = capture_user_input(func, :first_name, "retry")
+    assert result =~ "retry"
   end 
-
-  test "prints no errors message" do
-    assert capture_io(fn ->
-      ValidatingInputs.print_no_found_error_message
-    end) =~ "no errors found"
-  end
 
   test "runs through every category to validate inputs" do
-    assert capture_io("daisy\nmolving\n12345\nGG-6735", fn ->
-      ValidatingInputs.run_validation_checks
-    end) =~ "no errors found"
+    func = &ValidatingInputs.run_validation_checks/0
+    result = capture_final_output(func, "\ndaisy\nm\nmolving\nzip-error\n12345\nid-error\nGG-6735")
+    assert result =~ "no errors found"
   end
+
+  defp capture_output(func, category) do
+    capture_io(fn ->
+      func.(category)
+    end)
+  end
+
+  defp capture_invalid_message(func, invalid_input, retry_input, category) do
+    capture_io(retry_input, fn ->
+      func.(invalid_input, category)
+    end)
+  end
+
+  defp capture_user_input(func, category, user_input) do
+    capture_io([input: user_input, capture_prompt: false], fn ->
+      value = func.(user_input, category)
+      IO.write(value)
+    end)
+  end
+  
+  defp capture_final_output(func, user_inputs) do
+    capture_io(user_inputs, fn ->
+      func.()
+    end)
+  end
+
 end
