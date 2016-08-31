7fcd5b65fed1a707d2a8c6a2833cef184639f834
Daisy Molving <daisymolving@gmail.com>
2016-08-31 11:12:00 +0100

    make func variables, storing the function to be passed as an argument to capture_io functions, inline

--- test/validating_inputs_test.exs
+++ test/validating_inputs_test.exs
@@ -4,44 +4,37 @@ defmodule ValidatingInputsTest do
   doctest ValidatingInputs
 
   test "prints out each request" do
-    func = &ValidatingInputs.print_input_request/1
-    result = capture_output(func, [:first_name, :last_name, :zip_code, :employee_id])
+    result = capture_output(&ValidatingInputs.print_input_request/1, [:first_name, :last_name, :zip_code, :employee_id])
     assert result =~ "Please enter your"
   end
 
   test "a first name or last name must be at least 2 letters long" do 
-    func = &ValidatingInputs.validate_type/2
-    result = capture_invalid_message(func, "D\nM", "daisy\nmolving", [:first_name, :last_name])
+    result = capture_invalid_message(&ValidatingInputs.validate_type/2, "D\nM", "daisy\nmolving", [:first_name, :last_name])
     assert result =~ "must be at least two characters long"
   end 
 
   test "a zip code must be completely numerical" do
-    func = &ValidatingInputs.validate_type/2
-    result = capture_invalid_message(func, "ABC4E", "12345", [:zip_code])
+    result = capture_invalid_message(&ValidatingInputs.validate_type/2, "ABC4E", "12345", [:zip_code])
     assert result =~ "zip code must be numerical"
   end
 
   test "an employee id must start with two letters, then a hyphen, then four numbers" do
-    func = &ValidatingInputs.validate_type/2
-    result = capture_invalid_message(func, "AA1234", "ZZ-7890", [:employee_id])
+    result = capture_invalid_message(&ValidatingInputs.validate_type/2, "AA1234", "ZZ-7890", [:employee_id])
     assert result =~ "employee id must have two letters followed by a hyphen, then four numbers"
   end
 
   test "retry_for_empty_field message is printed when field is empty" do
-    func = &ValidatingInputs.validate_not_empty/2
-    result = capture_invalid_message(func, "\n\n\n", "daisy\nmolving\n12345\nRR-7382", [:first_name, :last_name, :zip_code, :employee_id])
+    result = capture_invalid_message(&ValidatingInputs.validate_not_empty/2, "\n\n\n", "daisy\nmolving\n12345\nRR-7382", [:first_name, :last_name, :zip_code, :employee_id])
     assert result =~ "cannot be empty. Please try again: "
   end 
 
   test "retry_for_empty_field takes an input" do
-    func = &ValidatingInputs.validate_not_empty/2
-    result = capture_user_input(func, [:first_name, :last_name, :zip_code, :employee_id], "daisy\nmolving\n12345\nAA-12345")
+    result = capture_user_input(&ValidatingInputs.validate_not_empty/2, [:first_name, :last_name, :zip_code, :employee_id], "daisy\nmolving\n12345\nAA-12345")
     assert result =~ "daisy\nmolving\n12345\nAA-12345"
   end 
   
   test "runs through every category to validate inputs" do
-    func = &ValidatingInputs.run_validation_checks/0
-    result = capture_final_output(func, "\ndaisy\nm\nmolving\nzip-error\n12345\nid-error\nGG-6735")
+    result = capture_final_output(&ValidatingInputs.run_validation_checks/0, "\ndaisy\nm\nmolving\nzip-error\n12345\nid-error\nGG-6735")
     assert result =~ "no errors found"
   end
 
